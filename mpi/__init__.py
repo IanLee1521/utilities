28bf565f30a6d9b25bd6bc24ce5958a98a106161
Ian Lee <IanLee1521@gmail.com>
2014-09-26 00:25:02 -0700

    Add function to get the host-rank mapping of mpi tasks

--- mpi/__init__.py
+++ mpi/__init__.py
@@ -14,5 +14,18 @@ def get_rank():
     return comm.Get_rank()
 
 
+def host_rank_mapping():
+    """Get host to rank mapping
+
+    Return dictionary mapping ranks to host
+    """
+    d = {}
+    for (host, rank) in comm.allgather((get_host(), get_rank())):
+        if host not in d:
+            d[host] = []
+        d[host].append(rank)
+    return d
+
+
 def finalize():
     return MPI.Finalize()
