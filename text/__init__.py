1fbb58a3f6692a7467f758ccedca6f8baa96a165
Ian Lee <IanLee1521@gmail.com>
2014-11-15 00:22:27 -0800

    Add function to get the functions / classes that are defined

--- text/__init__.py
+++ text/__init__.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import os
+import re
 
 
 def get_files(path, ext=None):
@@ -23,3 +24,28 @@ def blob_text(filenames):
     Create a blob of text by reading in all filenames into a string
     """
     return '\n'.join([open(fname).read() for fname in filenames])
+
+
+def get_definition(text, startswith):
+    """
+    Parse text to retrieve the definitions that start with keyword
+    """
+    return [
+        re.split('[ ()]', line.strip())[1]
+        for line in [line.strip() for line in text.splitlines()]
+        if line.startswith(startswith)
+        ]
+
+
+def get_functions(text, startswith='def '):
+    """
+    Parse text to retrive the functions and methods defined
+    """
+    return get_definition(text, startswith)
+
+
+def get_classes(text, startswith='class '):
+    """
+    Parse text to retrive the functions and methods defined
+    """
+    return get_definition(text, startswith)
