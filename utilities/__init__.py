656780b827202fc08992321ec2a98e91cb02da3b
Ian Lee <IanLee1521@gmail.com>
2014-11-18 23:50:55 -0800

    Add a wrapper to get just stdout back

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -3,8 +3,15 @@
 from subprocess import Popen, PIPE
 
 
-def _popen(cmd):
+def launch(cmd):
     """
     Fork the specified command, returning a tuple of (stdout, stderr)
     """
     return Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+
+
+def get_stdout(cmd):
+    """
+    Fork the specified command, returning stdout
+    """
+    return launch(cmd)[0]
