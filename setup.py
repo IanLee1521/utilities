8c129c3047a13ad55317fba647b3a8c1b34f4506
Ian Lee <IanLee1521@gmail.com>
2015-02-07 21:09:13 -0800

    Added setup.py file to allow installing module and find_duplicates entry point

---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='utilities',
+    version='0.1',
+    description='Utilities for common development tasks',
+    author='Ian Lee',
+    author_email='IanLee1521@gmail.com',
+    license='MIT License',
+    packages=['utilities'],
+    entry_points={
+        'console_scripts': [
+            'find_duplicates = utilities.find_duplicates:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
