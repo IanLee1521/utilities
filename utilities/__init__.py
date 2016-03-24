2ca6f61f4df00dc9e9f442dcc9fe243c4f03a707
Ian Lee <IanLee1521@gmail.com>
2016-03-23 21:45:53 -0700

    Added mkdir_p helper function from Stack Overflow

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 
+import errno
+import os
+
 from subprocess import Popen, PIPE
 
 
@@ -22,3 +25,18 @@ def get_stderr(cmd):
     Fork the specified command, returning stderr
     """
     return popen(cmd)[1]
+
+
+def mkdir_p(path):
+    """
+    Python version of shell `mkdir -p`
+
+    Taken from http://stackoverflow.com/a/600612/450858
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
